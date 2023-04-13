3641a0eab74a78519879f1c67a691836283a1471
CyBeRnErD <tom_ski18@yahoo.com>
2023-04-13 21:23:37 +0200

    Adding 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Angel!");
+
+int a = int.Parse(Console.ReadLine());
+
+int suma = a + 2;
+
+Console.WriteLine(suma);
